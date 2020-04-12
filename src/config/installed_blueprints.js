4d8a3baf14f7b3ea8024f7da428423fdc82c743f
Crowdbotics <team@crowdbotics.com>
2020-04-12 04:45:29 +0000

    add package blueprint-react-native-camera-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Camera28190', human_name: 'Camera', access_route: 'Camera28190', icon: 'camera'},
 { name: 'CalendarView28189', human_name: 'Calendar View', access_route: 'CalendarView28189', icon: 'calendar'},
 { name: 'Maps28188', human_name: 'Maps', access_route: 'Maps28188', icon: 'map'},
 { name: 'Tutorial28187', human_name: 'Tutorial', access_route: 'Tutorial28187', icon: 'question-circle'},
